34410a2779dc07fc191a044a7d66883f7c3974c6
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-07 03:33:59 -0400

    Add files via upload

---
+++ 3. Write a program to swap values of two int variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,temp;
+    printf("Enter The x :");
+    scanf("%d",&x);
+    printf("Enter The y :");
+    scanf("%d",&y);
+    temp=x; 
+    x=y;
+    y=temp;
+    printf(" x=%d \n y=%d",x,y);
+    return 0;
+
+}
