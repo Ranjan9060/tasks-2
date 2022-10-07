2de0181cccba295db7ed236cf47698e66de1ff4e
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-07 11:24:23 -0400

    Add files via upload

---
+++ 8. Write a program to check whether the given number is even or odd using a bitwise operator.c
@@ -0,0 +1,19 @@
+#include<stdio.H>
+int main()
+{
+    int num;
+
+    printf("Enter a Number :");
+
+    scanf("%d",&num);
+
+    if(num&=1)
+
+    printf("Odd Number");
+
+    else
+
+    printf("Even Number");
+
+    return 0;
+}
