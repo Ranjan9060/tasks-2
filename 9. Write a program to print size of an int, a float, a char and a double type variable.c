7722dcc3985d9c3a55aad6fd97292959e410a1d9
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-07 11:33:27 -0400

    Add files via upload

---
+++ 9. Write a program to print size of an int, a float, a char and a double type variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+    printf("int  Size is : %d\n",sizeof(a));
+    printf("char  Size is :%d\n",sizeof(b));
+    printf("float Size is: %d\n",sizeof(c));
+    printf("Double Size is:%d\n",sizeof(d));
+    return 0;
+}
